102cd32d2d8d744828495c00b661eda29565af6e
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2017-06-12 20:30:27 -0700

    resourceful: Improved daemon error handling

--- cmd/resourceful/daemon.go
+++ cmd/resourceful/daemon.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"errors"
 	"log"
 	"os"
 	"os/signal"
@@ -19,24 +18,50 @@ func daemon(args []string) {
 
 	log.Println("Starting resourceful guardian daemon")
 
+	// Detect the working directory, which is the source of policy files
 	wd, err := os.Getwd()
 	if err != nil {
-		log.Print(errors.New("unable to detect working directory"))
+		log.Printf("Unable to detect working directory: %v", err)
+		os.Exit(1)
 	}
 	log.Printf("Policy source directory: %s\n", wd)
 
-	ctx, cancel := context.WithCancel(context.Background())
-	go func() {
-		waitForSignal()
-		cancel()
-	}()
 	cfg := guardian.ServerConfig{
 		ListenSpec:     ":5877",
 		PolicyProvider: cacheprov.New(fsprov.New(wd)),
 		LeaseProvider:  memprov.New(),
 	}
+
+	// Verify that we're starting with a good policy set
+	policies, err := cfg.PolicyProvider.Policies()
+	if err != nil {
+		log.Printf("Failed to load policy set: %v", err)
+		os.Exit(1)
+	}
+
+	count := len(policies)
+	switch count {
+	case 1:
+		log.Printf("1 policy loaded")
+	default:
+		log.Printf("%d policies loaded", count)
+	}
+
+	ctx, shutdown := context.WithCancel(context.Background())
+	defer shutdown()
+	go func() {
+		waitForSignal()
+		shutdown()
+	}()
+
 	err = guardian.Run(ctx, cfg)
-	log.Print(err)
+
+	if err != nil {
+		log.Printf("Guardian server error: %v", err)
+		os.Exit(1)
+	}
+
+	log.Printf("Stopped resourceful guardian daemon")
 }
 
 func waitForSignal() {
