6a571303719424402755f6ee35ac5c6f941122fd
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2017-06-12 19:10:42 -0700

    resourceful: removed leftover function

--- cmd/resourceful/dialog.go
+++ cmd/resourceful/dialog.go
@@ -2,8 +2,4 @@
 
 package main
 
-import "github.com/scjalliance/resourceful/guardian/transport"
-
 func msgBox(title, msg string) {}
-
-func leaseRejectedDlg(program string, response transport.AcquireResponse) {}
