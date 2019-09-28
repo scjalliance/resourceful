092abeb174fa323331420da3d53563ca5e96f27a
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2019-09-27 17:29:42 -0700

    enforcer: Added static blacklist for additional safety

--- enforcer/blacklist_windows.go
+++ enforcer/blacklist_windows.go
@@ -2,10 +2,58 @@
 
 package enforcer
 
+import "strings"
+
+var blacklist = map[string]bool{
+	"applicationframehost":    true,
+	"chrome":                  true,
+	"cmd":                     true,
+	"csrss":                   true,
+	"conhost":                 true,
+	"ctfmon":                  true,
+	"dllhost":                 true,
+	"dwm":                     true,
+	"explorer":                true,
+	"lockapp":                 true,
+	"lsass":                   true,
+	"mmc":                     true,
+	"regedit":                 true,
+	"registry":                true,
+	"resourceful":             true,
+	"runtimebroker":           true,
+	"rtkauduservice64":        true,
+	"searchindexer":           true,
+	"searchfilterhost":        true,
+	"searchprotocolhost":      true,
+	"searchui":                true,
+	"securityhealthsystray":   true,
+	"services":                true,
+	"shellexperiencehost":     true,
+	"sihost":                  true,
+	"svchost":                 true,
+	"systemsettings":          true,
+	"systemsettingsbroker":    true,
+	"startmenuexperiencehost": true,
+	"taskhostw":               true,
+	"taskmgr":                 true,
+	"thunderbolt":             true,
+	"wininit":                 true,
+	"winlogon":                true,
+	"wudfhost":                true,
+	"windowsinternal.composableshell.experiences.textinput.inputapp": true,
+}
+
 // Blacklisted returns true if p is a blacklisted process that must not
 // be managed.
 func Blacklisted(p ProcessData) bool {
-	return p.Protected()
+	if p.Protected() {
+		return true
+	}
+
+	name := strings.ToLower(p.Name)
+	name = strings.TrimSuffix(name, ".exe")
+
+	return blacklist[name]
 
 	// TODO: Skip anything running as something other than the session's user
 
